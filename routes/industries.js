0522077d9d21246cf8bf3600d34284068187ef47
JianWXu <jianwxu88@gmail.com>
2023-11-15 14:51:53 -0800

    Update industries.js
    added get /:code to route that associates every company to the industry code queried

--- routes/industries.js
+++ routes/industries.js
@@ -24,6 +24,34 @@ router.post("/", async function (req, res, next) {
   }
 });
 
+router.get("/:code", async function (req, res, next) {
+  try {
+    const result = await db.query(
+      `SELECT i.code, i.industry, c.name 
+        FROM industries AS i 
+            LEFT JOIN sector 
+                ON i.code = ind_code 
+            LEFT JOIN companies AS c 
+                ON c.code = comp_code 
+        WHERE $1 = i.code`,
+      [req.params.code]
+    );
 
+    let { code, industry } = result.rows[0];
+    let names = result.rows.map((r) => r.name);
+
+    if (result.rows.length === 0) {
+      let notFoundError = new Error(
+        `There is no company with id '${req.params.code}`
+      );
+      notFoundError.status = 404;
+      throw notFoundError;
+    }
+
+    return res.json({ code, industry, names });
+  } catch (e) {
+    return next(e);
+  }
+});
 
 module.exports = router;
